b18e70fdddbfd568ee0fcd67340594f91dbaf6d4
Diego Lima <lima@rarolabs.com.br>
2025-08-29 18:09:31 -0300

    ajuste lib rarui

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "@rarui/components/dist/index.js";
-import "@rarui/styles/dist/index.css";
+import "@rarui/styles/dist/themes/globals.css";
+import "@rarui/styles/dist/themes/rarui-theme-dark.css";
 import "./controllers"
